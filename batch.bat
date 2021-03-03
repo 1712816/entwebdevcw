28af68926b819b18c3f36e3db6f544775dd22e8b
corsejack <34166649+1712816@users.noreply.github.com>
2021-03-03 23:00:28 +0000

    Deleting the Batch File

--- batch.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-set PORT = 8080
-set JWT_SECRET="mysecret"
-set MONGODB_URI="mongodb+srv://corsejack:z6Xpa7dteJCID4Tn@cluster0.hoh0l.mongodb.net/EntWebDB?retryWrites=true&w=majority"
-npm run-script development 
