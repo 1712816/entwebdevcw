3a13d24a23e3e81ef97a4f453ce1d04984958b23
1712816 <corsejack@hotmail.co.uk>
2021-03-04 01:56:08 +0000

    01.55 Fully functional backend for a web system.. probably

--- server/express.js
+++ server/express.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import Template from './/./../template'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express()
 // parse body params and attache them to req.body
@@ -19,6 +20,17 @@ app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+app.use((err, req, res, next) => {
+ if (err.name === 'UnauthorizedError') {
+ res.status(401).json({"error" : err.name + ": " + err.message})
+ }else if (err) {
+ res.status(400).json({"error" : err.name + ": " + err.message})
+ console.log(err)
+ }
+})
+
 
 app.get('/', (req, res) => {
  res.status(200).send(Template())
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userCtrl from '../controllers/user.controller'
+import authCtrl from '../controllers/auth.controller'
 
 const router = express.Router()
 
@@ -11,6 +12,11 @@ router.route('/api/users/:userId')
   .get(userCtrl.read)
   .put(userCtrl.update)
   .delete(userCtrl.remove)
+
+router.route('/api/users/:userId')
+  .get(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
 
 router.param('userId', userCtrl.userByID)
 
