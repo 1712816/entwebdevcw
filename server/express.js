00219ebb6665ba0d862e113cc6eaa97375af8f1e
1712816 <corsejack@hotmail.co.uk>
2021-03-04 16:29:21 +0000

    16.28 Halfway through lab 6 working on front end

--- server/express.js
+++ server/express.js
@@ -8,8 +8,22 @@ import helmet from 'helmet'
 import Template from './/./../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+import devBundle from './devBundle'
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
+import theme from './../client/theme'
 
+
+
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express()
+//devBundle compilation
+devBundle.compile(app)
+
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,9 +33,13 @@ app.use(compress())
 app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
+//enable userRoutes for logins
 app.use('/', userRoutes)
+//enable authRoutes for logins
 app.use('/', authRoutes)
-
+//
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+//
 app.use((err, req, res, next) => {
  if (err.name === 'UnauthorizedError') {
  res.status(401).json({"error" : err.name + ": " + err.message})
@@ -30,9 +48,29 @@ app.use((err, req, res, next) => {
  console.log(err)
  }
 })
-
-
-app.get('/', (req, res) => {
- res.status(200).send(Template())
+//
+app.get('*', (req, res) => {
+ const sheets = new ServerStyleSheets()
+ const context = {}
+ const markup = ReactDOMServer.renderToString(
+ sheets.collect(
+ <StaticRouter location={req.url} context={context}>
+ <ThemeProvider theme={theme}>
+ <MainRouter />
+ </ThemeProvider>
+ </StaticRouter>
+ )
+ )
+ if (context.url) {
+ return res.redirect(303, context.url)
+ }
+ const css = sheets.toString()
+ res.status(200).send(Template({
+ markup: markup,
+ css: css
+ }))
 })
+
+
+
 export default app
--- template.js
+++ template.js
@@ -1,12 +1,26 @@
-export default () => {
+export default ({markup, css}) => {
  return `<!doctype html>
  <html lang="en">
  <head>
  <meta charset="utf-8">
- <title> Enterprise Web Dev </title>
+ <meta
+ name="viewport"
+ content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+ >
+ <title> Lab Six </title>
+ <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+ <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+ <style>
+ a{
+ text-decoration: none;
+ color: #061d95
+ }
+ </style>
  </head>
- <body style="margin:0">
- <div id="root">Hello World</div>
+ <body>
+ <div id="root">${markup}</div>
+ <style id="jss-server-side">${css}</style>
+ <script type="text/javascript" src="/dist/bundle.js"></script>
  </body>
  </html>`
-}
+}
