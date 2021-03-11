8dd216e064c6a254d622f0bd4472484a4005999e
1712816 <corsejack@hotmail.co.uk>
2021-03-11 19:29:48 +0000

    potental fix to about setion 19.29 11.3.21

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -101,9 +101,10 @@ export default function EditProfile({ match }) {
             Edit Profile
           </Typography>
           <TextField id="name" label="Name" className={classes.textField} value={values.name} onChange={handleChange('name')} margin="normal"/><br/>
+          <TextField id="multiline-flexible" label="About" multiline rows="2" value={values.about} onChange={handleChange('about')} className={classes.textField} margin="normal"/><br/>
           <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal"/><br/>
           <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/><br/>
-          <TextField id="multiline-flexible" label="About" multiline rows="2" value={values.about} onChange={handleChange('about')} className={classes.textField} margin="normal"/><br/>
+
           {
             values.error && (<Typography component="p" color="error">
               <Icon color="error" className={classes.error}>error</Icon>
