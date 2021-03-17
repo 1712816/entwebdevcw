ad919433ff00eac42da05d30028b85836998b049
1712816 <corsejack@hotmail.co.uk>
2021-03-17 16:20:49 +0000

    removing users page as it is not required anymore 16.20 17.3.21

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
-import Users from './user/Users'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
 import EditProfile from './user/EditProfile'
@@ -18,7 +17,6 @@ const MainRouter = () => {
  <Menu/>
  <Switch>
  <Route exact path="/" component={Home}/>
- <Route path="/users" component={Users}/>
  <Route path="/signup" component={Signup}/>
  <Route path="/signin" component={Signin}/>
  <Route path="/Products" component={Products}/>
